8da435daa58f5878454766882c58842358bf404d
Jorisson Vitor de Carvalho Sabino <52410774+Jorisson@users.noreply.github.com>
2023-06-05 10:03:46 -0300

    Update fireBaseConection.js
    Ajustes na estrutura de conexão

--- src/services/fireBaseConection.js
+++ src/services/fireBaseConection.js
@@ -3,13 +3,13 @@ import {getFirestore} from 'firebase/firestore'
 import {getAuth} from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAw9iYJzww_MHdd_B9JQ-dFvPAkzY815_k",
+  apiKey: KEY_API,
   authDomain: "cartaovisita-c3d4d.firebaseapp.com",
   projectId: "cartaovisita-c3d4d",
   storageBucket: "cartaovisita-c3d4d.appspot.com",
-  messagingSenderId: "840991502387",
-  appId: "1:840991502387:web:9766f0265d0cefdf26f346",
-  measurementId: "G-7GKVRCNFWF"
+  messagingSenderId: MESSAGESENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASURE_ID
 };
 
 const firebaseapp = initializeApp(firebaseConfig);
